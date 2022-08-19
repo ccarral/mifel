76b19a59d66d88a64509b854ffed99841afcc1ac
Carlos Carral C <carloscarral13@gmail.com>
2022-08-19 08:34:15 -0500

    Define clase UsuarioService

---
+++ src/main/java/com/mifelusers/service/UsuarioService.java
@@ -0,0 +1,16 @@
+package com.mifelusers.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioService {
+    public List<Usuario> getUsuariosPorNombre(String nombre) {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        return usuarios;
+    }
+
+    public List<Usuario> getAllUsuarios() {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        return usuarios;
+    }
+}
